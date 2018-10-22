3d9d5e9104f1c294de4bd5b31bd01f6d684e3465
Turchin Alexander <alexanderturchin1@gmail.com>
2018-10-22 14:17:56 +0300

    Add day 01 task with tests

---
+++ NET1.A.2018.Turchin.01/Sorts.Tests/SortClassTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NET1.A._2018.Turchin._01.Tests
+{
+    [TestClass]
+    public class SortClassTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void MergeMethod_Null_ReturnThrowArgumentNullException()
+        {
+            Sort.Merge(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void MergeMethod_ZeroLenghtArray_ReturnThrowArgumentNullException()
+        {
+            Sort.Merge(new int[] { });
+        }
+
+        [TestMethod]
+        public void MergeMethod_SmallUnsortedArray_ReturnSortedArray()
+        {
+            int[] array = new int[] { 1, 2, 21, 19, 0, -1 };
+
+            Sort.Merge(array);
+
+            Assert.IsTrue(CheckArrayNotDecreasing(array));
+        }
+
+        [TestMethod]
+        public void MergeMethod_BigRandomArray_ReturnSortedArray()
+        {
+			int[] array = RandomBigArray();
+		
+            Sort.Merge(array);
+
+            Assert.IsTrue(CheckArrayNotDecreasing(array));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void QuickMethod_Null_ReturnThrowArgumentNullException()
+        {
+            Sort.Quick(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void QuickMethod_ZeroLenghtArray_ReturnThrowArgumentNullException()
+        {
+            Sort.Quick(new int[] { });
+        }
+
+        [TestMethod]
+        public void QuickMethod_SmallUnsortedArray_ReturnSortedArray()
+        {
+            int[] array = new int[] { 1, 2, 21, 19, 0, -1 };
+
+            Sort.Quick(array);
+
+            Assert.IsTrue(CheckArrayNotDecreasing(array));
+        }
+
+        [TestMethod]
+        public void QuickMethod_BigRandomArray_ReturnSortedArray()
+		{
+			int[] array = RandomBigArray();
+
+            Sort.Quick(array);
+
+            Assert.IsTrue(CheckArrayNotDecreasing(array));
+        }
+
+		/// <summary>
+		/// Create a big random array.
+		/// </summary>
+		/// <returns>The pointer to the array.</returns>
+		private int[] RandomBigArray()
+		{
+			const int Min = -100;
+			const int Max = 100;
+
+			int[] array = new int[100000];
+
+			Random randNum = new Random();
+			for (int i = 0; i < array.Length; i++)
+			{
+				array[i] = randNum.Next(Min, Max);
+			}
+
+			return array;
+		}
+
+		/// <summary>
+		/// Check that the sequence of the array is not decreasing
+		/// </summary>
+		/// <param name="array">The array to be checked.</param>
+		/// <returns>If the array is sorted.</returns>
+		private bool CheckArrayNotDecreasing(int[] array)
+		{
+			bool isNotDecreasing = true;
+
+			for (int i = 0; (i < array.Length - 1) && isNotDecreasing; i++)
+			{
+				isNotDecreasing = (array[i] <= array[i + 1]);
+			}
+
+			return isNotDecreasing;
+		}
+	}
+}
---
+++ NET1.A.2018.Turchin.01/Sorts/Sort.cs
@@ -0,0 +1,214 @@
+﻿using System;
+
+namespace NET1.A._2018.Turchin._01
+{
+    /// <summary>
+    /// Represent the class, which contains Merge and Quick sort algorithms.
+    /// </summary>
+    public static class Sort
+    {
+        /// <summary>
+        /// Represent the merge sort.
+        /// </summary>
+        /// <param name="array">Input array.</param>
+		/// <exception cref="ArgumentException">If the length of the array is zero.</exception>
+		/// <exception cref="ArgumentNullException">If array is null</exception>
+        public static void Merge(int[] array)
+        {
+            CheckConditions(array);
+			MergeLogic(array, 0, array.Length - 1);
+        }
+
+		/// <summary>
+		/// Represent the merge sort.
+		/// </summary>
+		/// <param name="array">Input array.</param>
+		/// <param name="left">The left bound of the array.</param>
+		/// <param name="right">The right bound of the array.</param>
+		/// <exception cref="ArgumentException">If the length of the array is zero.</exception>
+		/// <exception cref="ArgumentNullException">If array is null</exception>
+		public static void Merge(int[] array, int left, int right)
+		{
+			CheckConditions(array, left, right);
+			MergeLogic(array, left, right);
+		}
+
+		/// <summary>
+		/// Represent the Quick sort.
+		/// </summary>
+		/// <param name="array">Input array.</param>
+		/// <exception cref="ArgumentException">If the length of the array is zero.</exception>
+		/// <exception cref="ArgumentNullException">If array is null</exception>
+		public static void Quick(int[] array)
+		{
+			CheckConditions(array);
+			QuickLogic(array, 0, array.Length - 1);
+		}
+
+		/// <summary>
+		/// Represent the Quick sort.
+		/// </summary>
+		/// <param name="array">Input array.</param>
+		/// <param name="left">The left bound of the array.</param>
+		/// <param name="right">The right bound of the array.</param>
+		/// <exception cref="ArgumentException">If the length of the array is zero.</exception>
+		/// <exception cref="ArgumentNullException">If array is null</exception>
+		public static void Quick(int[] array, int left, int right)
+		{
+			CheckConditions(array, left, right);
+			QuickLogic(array, left, right);
+		}
+
+		/// <summary>
+		/// Divide array to two little ones.
+		/// </summary>
+		/// <param name="array">Input array.</param>
+		/// <param name="left">The left bound of the array.</param>
+		/// <param name="right">The right bound of the array.</param>
+		private static void MergeLogic(int[] array, int left, int right)
+		{
+			int middle = (left + right) / 2;
+
+			if (left == right)
+			{
+				return;
+			}
+
+			MergeLogic(array, left, middle);
+			MergeLogic(array, middle + 1, right);
+
+			MergeLogic(array, left, middle, right);
+		}
+
+		/// <summary>
+		/// Take two arrays in a sorted one. 
+		/// </summary>
+		/// <param name="array">Two logic arrays in one physic array.</param>
+		/// <param name="left">The left bound of the first array.</param>
+		/// <param name="middle">The right bound of the first array and the left bound of the second one.</param>
+		/// <param name="right">The right bound of the second array.</param>
+		private static void MergeLogic(int[] array, int left, int middle, int right)
+		{
+			int firstArray = left;
+			int secondArray = middle + 1;
+			int[] temp = new int[right - left + 1];
+			int tempIndex = 0;
+
+			while ((firstArray <= middle) && (secondArray <= right))
+			{
+				temp[tempIndex] = (array[firstArray] > array[secondArray])
+					? array[secondArray++]
+					: array[firstArray++];
+				tempIndex++;
+			}
+
+			while (firstArray <= middle)
+			{
+				temp[tempIndex++] = array[firstArray++];
+			}
+
+			while (secondArray <= right)
+			{
+				temp[tempIndex++] = array[secondArray++];
+			}
+
+			for (int i = 0; i < temp.Length; i++)
+			{
+				array[left + i] = temp[i];
+			}
+		}
+
+        /// <summary>
+        /// Sort the array with Quick sort algorithm.
+        /// </summary>
+        /// <param name="arr">Input array.</param>
+        /// <param name="left">The left bound of the array.</param>
+        /// <param name="right">The right bound </param>
+        private static void QuickLogic(int[] arr, int left, int right)
+        {
+            int i = left, j = right;
+            int x = arr[(left + right) / 2];
+
+            do
+            {
+                while (arr[i] < x)
+                {
+                    i++;
+                }
+
+                while (arr[j] > x)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+					Swap(ref arr[i], ref arr[j]);
+
+                    i++;
+                    j--;
+                }
+            }
+            while (i < j);
+
+            if (left < j)
+            {
+                Quick(arr, left, j);
+            }
+
+            if (i < right)
+            {
+                Quick(arr, i, right);
+            }
+        }
+
+		/// <summary>
+		/// Changes values between two arguments.
+		/// </summary>
+		/// <param name="value1">Argument 1.</param>
+		/// <param name="value2">Argument 2.</param>
+		private static void Swap(ref int value1, ref int value2)
+		{
+			int temp = value1;
+			value1 = value2;
+			value2 = temp;
+		}
+
+		/// <summary>
+		/// Check conditions of the input array.
+		/// </summary>
+		/// <param name="array">Input array.</param>
+		/// <exception cref="ArgumentException">If the length of the array is zero.</exception>
+		/// <exception cref="ArgumentNullException">If array is null</exception>
+		private static void CheckConditions(int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+			if (array.Length == 0)
+			{
+				throw new ArgumentException(nameof(array));
+			}
+        }
+
+		/// <summary>
+		/// Check conditions of the input array.
+		/// </summary>
+		/// <param name="array">The input array.</param>
+		/// <param name="left">The left bound of the array.</param>
+		/// <param name="right">The right bound of the array.</param>
+		/// <exception cref="ArgumentException">If the length of the array is zero or the bound are invalid.</exception>
+		/// <exception cref="ArgumentNullException">If array is null</exception>
+		private static void CheckConditions(int[] array, int left, int right)
+		{
+			CheckConditions(array);
+
+			if ((left < 0) || (right < 0) || (left > right))
+			{
+				throw new ArgumentException();
+			}
+		}
+    }
+}
