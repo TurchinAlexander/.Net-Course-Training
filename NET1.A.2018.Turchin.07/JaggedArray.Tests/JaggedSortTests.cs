763155d49d3409d4ea60d4163542d2340eab4014
Turchin Alexander <alexanderturchin1@gmail.com>
2018-10-24 22:32:09 +0300

    Add day 07 task with NUnit tests.

---
+++ NET1.A.2018.Turchin.07/JaggedArray.Tests/JaggedSortTests.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using NUnit.Framework;
+using JaggedArray;
+
+namespace JaggedArray.Tests
+{
+	[TestFixture]
+    public class JaggedSortTests
+    {
+		[Test]
+		public void ExecuteMethod_NullArray_ReturnArgumentNullException()
+		{
+			Assert.Throws<ArgumentNullException>(() => JaggedSort.Execute(null, JaggedSort.CompareBy.Sum, JaggedSort.TypeSort.Ascending));
+		}
+
+		[Test]
+		public void ExecuteMethod_EmptyArray_ReturnArgumentException()
+		{
+			Assert.Throws<ArgumentException>(() => JaggedSort.Execute(new int[][] { }, JaggedSort.CompareBy.Sum, JaggedSort.TypeSort.Ascending));
+		}
+
+		[Test]
+		public void ExecuteMethod_ArrayWithNullElements_ReturnArgumentNullException()
+		{
+			int[][] array = new int[][]
+			{
+				new int[] { 1, 2, 3 },
+				null,
+				new int[] {5, 6, 7 }
+			};
+
+			Assert.Throws<ArgumentNullException>(() => JaggedSort.Execute(array, JaggedSort.CompareBy.Sum, JaggedSort.TypeSort.Ascending));
+		}
+
+		[Test]
+		public void ExecuteMethod_ArrayWithEmptyElements_ReturnArgumentException()
+		{
+			int[][] array = new int[][]
+			{
+				new int[] { 1, 2, 3 },
+				new int[] { },
+				new int[] {5, 6, 7 }
+			};
+
+			Assert.Throws<ArgumentException>(() => JaggedSort.Execute(array, JaggedSort.CompareBy.Sum, JaggedSort.TypeSort.Ascending));
+		}
+
+		[Test]
+		public void ExecuteMethod_ArraySortBySumAndAscending_ReturnSortedArray()
+		{
+			int[][] array = new int[][]
+			{
+				new int[] {1, 2, 3},
+				new int[] {5, 6, 7},
+				new int[] {0, 1, 2}
+			};
+
+			int[][] expected = new int[][]
+			{
+				new int[] {0, 1, 2},
+				new int[] {1, 2, 3},
+				new int[] {5, 6, 7}
+			};
+
+			JaggedSort.Execute(array, JaggedSort.CompareBy.Sum, JaggedSort.TypeSort.Ascending);
+
+			Assert.IsTrue(CheckJaggedArrays(expected, array));
+		}
+
+		[Test]
+		public void ExecuteMethod_ArraySortBySumAndDescending_ReturnSortedArray()
+		{
+			int[][] array = new int[][]
+			{
+				new int[] {1, 2, 3},
+				new int[] {5, 6, 7},
+				new int[] {0, 1, 2}
+			};
+
+			int[][] expected = new int[][]
+			{
+				new int[] {5, 6, 7},
+				new int[] {1, 2, 3},
+				new int[] {0, 1, 2}
+			};
+
+			JaggedSort.Execute(array, JaggedSort.CompareBy.Sum, JaggedSort.TypeSort.Descending);
+
+			Assert.IsTrue(CheckJaggedArrays(expected, array));
+		}
+
+		[Test]
+		public void ExecuteMethod_ArraySortByMaxValueAndAscending_ReturnSortedArray()
+		{
+			int[][] array = new int[][]
+			{
+				new int[] {1, 2, 10},
+				new int[] {5, 6, 7},
+				new int[] {0, 1, 2}
+			};
+
+			int[][] expected = new int[][]
+			{
+				new int[] {0, 1, 2},
+				new int[] {5, 6, 7},
+				new int[] {1, 2, 10}
+			};
+
+			JaggedSort.Execute(array, JaggedSort.CompareBy.MaxValue, JaggedSort.TypeSort.Ascending);
+
+			Assert.IsTrue(CheckJaggedArrays(expected, array));
+		}
+
+		[Test]
+		public void ExecuteMethod_ArraySortByMaxValueAndDescending_ReturnSortedArray()
+		{
+			int[][] array = new int[][]
+			{
+				new int[] {1, 2, 3},
+				new int[] {5, 6, 7},
+				new int[] {0, 1, 2}
+			};
+
+			int[][] expected = new int[][]
+			{
+				new int[] {5, 6, 7},
+				new int[] {1, 2, 3},
+				new int[] {0, 1, 2}
+			};
+
+			JaggedSort.Execute(array, JaggedSort.CompareBy.MaxValue, JaggedSort.TypeSort.Descending);
+
+			Assert.IsTrue(CheckJaggedArrays(expected, array));
+		}
+
+		[Test]
+		public void ExecuteMethod_ArraySortByMinValueAndAscending_ReturnSortedArray()
+		{
+			int[][] array = new int[][]
+			{
+				new int[] {11, -2, 3},
+				new int[] {5, 6, 7},
+				new int[] {0, -1, 2}
+			};
+
+			int[][] expected = new int[][]
+			{
+				new int[] {11, -2, 3},
+				new int[] {0, -1, 2},
+				new int[] {5, 6, 7}
+			};
+
+			JaggedSort.Execute(array, JaggedSort.CompareBy.MinValue, JaggedSort.TypeSort.Ascending);
+
+			Assert.IsTrue(CheckJaggedArrays(expected, array));
+		}
+
+		[Test]
+		public void ExecuteMethod_ArraySortByMinValueAndDescending_ReturnSortedArray()
+		{
+			int[][] array = new int[][]
+			{
+				new int[] {11, -2, 3},
+				new int[] {5, 6, 7},
+				new int[] {0, -1, 2}
+			};
+
+			int[][] expected = new int[][]
+			{
+				new int[] {5, 6, 7},
+				new int[] {0, -1, 2},
+				new int[] {11, -2, 3}
+				
+			};
+
+			JaggedSort.Execute(array, JaggedSort.CompareBy.MinValue, JaggedSort.TypeSort.Descending);
+
+			Assert.IsTrue(CheckJaggedArrays(expected, array));
+		}
+
+		private bool CheckJaggedArrays(int[][] expected, int[][] actual)
+		{
+			bool isEqual = true;
+
+			for (int i = 0; (i < expected.Length) && isEqual; i++) 
+				for (int j = 0; (j < expected[i].Length) && isEqual; j++)
+				{
+					isEqual = (expected[i][j] == actual[i][j]);
+				}
+
+			return isEqual;
+		}
+	}
+}
---
+++ NET1.A.2018.Turchin.07/JaggedArray/JaggedSort.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArray
+{
+    public static class JaggedSort
+    {
+		/// <summary>
+		/// What we should sort by.
+		/// </summary>
+		public enum CompareBy
+		{
+			Sum = 1,
+			MinValue = 2,
+			MaxValue = 3
+		};
+
+		/// <summary>
+		/// How we should sort.
+		/// </summary>
+		public enum TypeSort
+		{
+			Ascending = 1,
+			Descending = 2
+		}
+
+		/// <summary>
+		/// Sort jagged array.
+		/// </summary>
+		/// <param name="array">Input array.</param>
+		/// <param name="compare">What we sort by.</param>
+		/// <param name="typeSort">Type of sort.</param>
+		public static void Execute(int[][] array, CompareBy compare, TypeSort typeSort)
+		{
+			CheckArguments(array);
+
+			int[] keyValues = new int[array.Length];
+
+			FindKeyValues(keyValues, array, compare);
+
+			for (int i = 0; i < keyValues.Length - 1; i++)
+				for (int j = 0; j < keyValues.Length - i - 1; j++)
+				{
+					if (Compare(keyValues[j], keyValues[j + 1], typeSort))
+					{
+						Swap(ref keyValues[j], ref keyValues[j + 1]);
+						Swap(ref array[j], ref array[j + 1]);
+					}
+				}
+		}
+
+		/// <summary>
+		/// Compare two arguments.
+		/// </summary>
+		/// <param name="a">First argument.</param>
+		/// <param name="b">Second argument.</param>
+		/// <param name="typeSort">How we should compare.</param>
+		/// <returns></returns>
+		private static bool Compare(int a, int b, TypeSort typeSort)
+		{
+			bool shouldSwap = false;
+
+			if (typeSort == TypeSort.Ascending)
+			{
+				shouldSwap = (a > b);
+			} else if (typeSort == TypeSort.Descending)
+			{
+				shouldSwap = (a < b);
+			}
+
+			return shouldSwap;
+		}
+
+		/// <summary>
+		/// Swap values between themselfs.
+		/// </summary>
+		/// <param name="a">First value.</param>
+		/// <param name="b">Second value.</param>
+		private static void Swap<T>(ref T a, ref T b)
+		{
+			T temp = a;
+			a = b;
+			b = temp;
+		}
+
+		/// <summary>
+		/// Check input arguments for valid.
+		/// </summary>
+		/// <param name="array">Array to check.</param>
+		private static void CheckArguments(int[][] array)
+		{
+			if (array == null) throw new ArgumentNullException(nameof(array));
+			if (array.Length == 0) throw new ArgumentException(nameof(array));
+
+			foreach (int[] element in array)
+			{
+				if (element == null) throw new ArgumentNullException(nameof(array));
+				if (element.Length == 0) throw new ArgumentException(nameof(array));
+			}
+		}
+
+		/// <summary>
+		/// Find key values which what we will sort our jagged array.
+		/// </summary>
+		/// <param name="keyValues">Array of key values.</param>
+		/// <param name="array">Array from what we will take keyValues.</param>
+		/// <param name="compare">What we sort by.</param>
+		private static void FindKeyValues(int[] keyValues, int[][] array, CompareBy compare)
+		{
+			for (int i = 0; i < keyValues.Length; i++)
+			{
+				switch (compare)
+				{
+					case CompareBy.Sum:
+						keyValues[i] = array[i].Sum();
+						break;
+
+					case CompareBy.MaxValue:
+						keyValues[i] = array[i].Max();
+						break;
+
+					case CompareBy.MinValue:
+						keyValues[i] = array[i].Min();
+						break;
+				}
+			}
+		}
+    }
+}
