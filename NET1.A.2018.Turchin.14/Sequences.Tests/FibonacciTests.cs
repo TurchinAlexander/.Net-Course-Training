0997ff44b035ca7932795075bfe41ce1388a1e6d
Turchin Alexander <alexanderturchin1@gmail.com>
2018-11-10 23:00:05 +0300

    Add Fibonacci's sequence's generator.

---
+++ NET1.A.2018.Turchin.14/Sequences.Tests/FibonacciTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using NUnit.Framework;
+
+namespace Sequences.Tests
+{
+	[TestFixture]
+    public class FibonacciTests
+    {
+		static object[] RightCount = new object[]
+		{
+			new object[] { 1, "0" },
+			new object[] { 2, "0, 1" },
+			new object[] { 5, "0, 1, 1, 2, 3" },
+			new object[] { 10, "0, 1, 1, 2, 3, 5, 8, 13, 21, 34" }
+		};
+
+		static object[] RightRange = new object[]
+		{
+			new object[] { 0, 0, "0" },
+			new object[] { 0, 1, "0, 1" },
+			new object[] { 0, 4, "0, 1, 1, 2, 3" },
+			new object[] { 0, 9, "0, 1, 1, 2, 3, 5, 8, 13, 21, 34" },
+			new object[] { 1, 4, "1, 1, 2, 3" },
+			new object[] { 3, 4, "2, 3" },
+			new object[] { 8, 10, "21, 34, 55" }
+		};
+
+		[TestCaseSource("RightCount")]
+		public void GetSequenceMethod_RightCount_RightSequence(int count, string expected)
+		{
+			string actual = Fibonacci.GetSequence(count).ConvertToString();
+
+			Assert.IsTrue(expected.Equals(actual));
+		}
+
+		[TestCaseSource("RightRange")]
+		public void GetSequenceMethod_RightRange_RightSequence(int firstIndex, int secondIndex, string expected)
+		{
+			string actual = Fibonacci.GetSequence(firstIndex, secondIndex).ConvertToString();
+
+			Assert.IsTrue(expected.Equals(actual));
+		}
+
+		[TestCase(-1)]
+		[TestCase(0)]
+		public void GetSequenceMethod_WrondCount_Exception(int count)
+		{
+			Assert.Throws<ArgumentException>(() => Fibonacci.GetSequence(count));
+		}
+
+		[TestCase(-4, -1)]
+		[TestCase(-4, 0)]
+		[TestCase(5, -5)]
+		[TestCase(1, 0)]
+		[TestCase(4, 3)]
+		public void GetSequenceMethod_WrondRange_Exception(int firstIndex, int secondIndex)
+		{
+			Assert.Throws<ArgumentException>(() => Fibonacci.GetSequence(firstIndex, secondIndex));
+		}
+	}
+
+
+	public static class Coverts
+	{
+		public static string ConvertToString(this BigInteger[] array)
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				stringBuilder.Append(array[i]);
+				stringBuilder.Append(", ");
+			}
+
+			stringBuilder.Length -= 2;
+
+			return stringBuilder.ToString();
+		}
+	}
+}
---
+++ NET1.A.2018.Turchin.14/Sequences/Fibonacci.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Numerics;
+
+namespace Sequences
+{
+	public static class Fibonacci
+	{
+		/// <summary>
+		/// Get first numbers in Fibonacci's sequence.
+		/// </summary>
+		/// <param name="count">The count of Fibonacci's numbers.</param>
+		/// <returns>Array of Fibonacci's numbers.</returns>
+		public static BigInteger[] GetSequence(int count)
+		{
+			if (count <= 0) throw new ArgumentException($"{nameof(count)} cannot be negative or zero.");
+
+			return CreateSequence(0, count - 1);
+		}
+
+		/// <summary>
+		/// Get Fibonacci's sequence in current range.
+		/// </summary>
+		/// <param name="firstIndex">Start index of the element in Fibonacci's sequence.</param>
+		/// <param name="lastIndex">Last index of the element in Fibonacci's sequence.</param>
+		/// <returns>Array of Fibonacci's numbers.</returns>
+		public static BigInteger[] GetSequence(int firstIndex, int lastIndex)
+		{
+			if (firstIndex < 0) throw new ArgumentException($"{nameof(firstIndex)} cannot be negative.");
+			if (lastIndex < 0) throw new ArgumentException($"{nameof(lastIndex)} cannot be negative.");
+			if (firstIndex > lastIndex) throw new ArgumentException($"{nameof(firstIndex)} cannot be greater than {nameof(lastIndex)}.");
+
+			return CreateSequence(firstIndex, lastIndex);
+		}
+
+		private static BigInteger[] CreateSequence(int firstIndex, int lastIndex)
+		{
+			BigInteger[] sequence = new BigInteger[lastIndex - firstIndex + 1];
+			BigInteger prev = 0, current = 1, next;
+			int index = 0;
+
+			for (int i = 0; i <= lastIndex; i++)
+			{
+				if (i >= firstIndex)
+				{
+					sequence[index++] = prev;
+				}
+
+				next = prev + current;
+				prev = current;
+				current = next;
+			}
+
+			return sequence;
+		}
+	}
+}
