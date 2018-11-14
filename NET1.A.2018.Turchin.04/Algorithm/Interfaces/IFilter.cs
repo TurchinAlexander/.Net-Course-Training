140e87883db87025e1bf0ebd4a8c0323a978fc01
Turchin Alexander <alexanderturchin1@gmail.com>
2018-11-14 14:52:49 +0300

    Add Filter interface.

---
+++ NET1.A.2018.Turchin.04/Algorithm/Interfaces/IFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Interfaces
+{
+	/// <summary>
+	/// Interface to filter smth by pattern.
+	/// </summary>
+	/// <typeparam name="TSource">Input type.</typeparam>
+	public interface IFilter<in TSource>
+	{
+		bool IsValid(TSource source);
+	}
+}
